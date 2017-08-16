1d38e989d3c0313d1f2d835f14050456ba23952f
West Kraemer <kraemerwest@gmail.com>
2017-08-16 12:07:19 -0400

    add user ability to input place names

--- apiDemo/ViewController.swift
+++ apiDemo/ViewController.swift
@@ -10,18 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var cityTextField: UITextField!
-    
-    @IBAction func submit(_ sender: Any) {
-    }
     
     @IBOutlet var resultLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var cityTextField: UITextField!
+    
+    @IBAction func submit(_ sender: Any) {
         
-        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=e938df6166d02d7d30aa798c4ac23687")!
+        if let url = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=" + cityTextField.text! + ",uk&appid=e938df6166d02d7d30aa798c4ac23687")
+        
+        {
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
@@ -34,23 +32,23 @@ class ViewController: UIViewController {
                 if let urlContent = data {
                     
                     do {
-                    
-                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                    
-                    print(jsonResult)
+                        
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        
+                        print(jsonResult)
                         print(jsonResult["name"])
                         
                         if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
-                        
+                            
                             DispatchQueue.main.sync(execute: {
-                            
-                                    self.resultLabel.text = description
-                            
+                                
+                                self.resultLabel.text = description
+                                
                             })
-                        
+                            
                         }
                         
-                    
+                        
                     } catch {
                         
                         print("JSON Processing Failed")
@@ -59,12 +57,15 @@ class ViewController: UIViewController {
                 }
                 
             }
-            }
-        
-        task.resume()
-            
         }
         
+        task.resume()
+        
+        } else {
+            resultLabel.text = "Couldn't find weather for that city"
+        }
+    }
+
         
     }
 
