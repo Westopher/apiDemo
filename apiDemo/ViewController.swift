f7ac644d1e346490cfbd80f9cedcd7d96487a61e
West Kraemer <kraemerwest@gmail.com>
2017-08-16 10:56:05 -0400

    add api via JSON data and return JSON data by assigning it to a variable and printing the variable in the console

--- apiDemo/ViewController.swift
+++ apiDemo/ViewController.swift
@@ -14,16 +14,57 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=e938df6166d02d7d30aa798c4ac23687")
+        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=e938df6166d02d7d30aa798c4ac23687")!
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            if error != nil {
+                
+                print (error)
+                
+            } else {
+                
+                if let urlContent = data {
+                    
+                    do {
+                    
+                    let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                    
+                    print(jsonResult)
+                        print(jsonResult["name"])
+                        
+                        if let description = ((jsonResult["weather"] as? NSArray)?[0] as? NSDictionary)?["description"] as? String {
+                        
+                        print(description)
+                        
+                        }
+                        
+                    
+                    } catch {
+                        
+                        print("JSON Processing Failed")
+                    }
+                    
+                }
+                
+            }
+            }
+        
+        task.resume()
+            
+        }
         
         
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
-}
-
+//
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+//
+//
+//}
+//
