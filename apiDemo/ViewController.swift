5a071debbf34fc717db6a9cf7d279d572b7aa087
West Kraemer <kraemerwest@gmail.com>
2017-08-16 10:23:53 -0400

    allow http via info.plist file

--- apiDemo/ViewController.swift
+++ apiDemo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "http://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=e938df6166d02d7d30aa798c4ac23687")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
